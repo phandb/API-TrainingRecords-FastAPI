1c935a7b82fccdadcac5e79222768c436412c8d0
phandb72 <phandb72@gmail.com>
2022-04-24 08:51:35 -0500

    Read API

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
-from fastapi import FastAPI
-from database import engine
-import models
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
 
+from database import engine, SessionLocal
+import models
 
 app = FastAPI()
 
-
 models.Base.metadata.create_all(bind=engine)
 
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+'''
+# Create database at the beginning
 @app.get("/")
 async def create_database():
     return {"Database": "Created"}
+'''
+
+
+# ---------CRUD API-----------------
+@app.get("/")
+async def read_all(db: Session = Depends(get_db)):
+    return db.query(models.Tasks).all()
+
+
+@app.get("/task/{task_id}")
+async def read_task(task_id: int, db: Session = Depends(get_db)):
+    task_model = db.query(models.Tasks).filter(models.Tasks.id == task_id).first()
+
+    if task_model is not None:
+        return task_model
+    raise http_exception()
+
+
+def http_exception():
+    return HTTPException(status_code=404, detail="Task not found")
