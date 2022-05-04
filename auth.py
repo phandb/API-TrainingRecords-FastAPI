8fd8d83afb30ca188d93a73d9e381eb5e99b5db9
phandb72 <phandb72@gmail.com>
2022-05-03 22:52:13 -0500

    Create Access Token for User

--- auth.py
+++ auth.py
@@ -1,12 +1,21 @@
-from fastapi import FastAPI, Depends
+from datetime import timedelta, datetime
+
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from jose import jwt
 from passlib.context import CryptContext
 from pydantic import BaseModel
 from typing import Optional
 
 from sqlalchemy.orm import Session
+from starlette.responses import Response
 
 import models
 from database import engine, SessionLocal
+
+# secret key generated using 256-bit
+SECRET_KEY = "ShVmYq3t6w9z$C&F)J@NcRfTjWnZr4u7"
+ALGORITHM = "HS256"
 
 
 class CreateUser(BaseModel):
@@ -21,6 +30,8 @@ bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 models.Base.metadata.create_all(bind=engine)
 
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="token")
+
 app = FastAPI()
 
 
@@ -34,6 +45,35 @@ def get_db():
 
 def get_password_hashed(password):
     return bcrypt_context.hash(password)
+
+
+def verify_password(plain_password, hashed_password):
+    return bcrypt_context.verify(plain_password, hashed_password)
+
+
+def authenticate_user(username: str, password: str, db):
+    user = db.query(models.Users).filter(models.Users.username == username).first()
+
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+
+    return user
+
+
+def create_access_token(username: str, user_id: int,
+                        expires_delta: Optional[timedelta] = None):
+    encode = {"sub": username, "id": user_id}
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+
+    else:
+        expire = datetime.utcnow() + timedelta(minute=15)
+
+    encode.update({"exp": expire})
+
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
 @app.post("/create/user")
@@ -53,3 +93,19 @@ async def create_new_user(create_user: CreateUser, db: Session = Depends(get_db)
 
     # return create_user_model
 
+
+# method to return JSON web token
+@app.post("/token")
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(),
+                                 db: Session = Depends(get_db)):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    # return "User Validated!"
+    token_expires = timedelta(minutes=60)
+    token = create_access_token(user.username,
+                                user.id,
+                                expires_delta=token_expires)
+
+    return {"token":  token}
