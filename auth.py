561d3c21da1693d78ab56f0402e24de1a3f14abf
phandb72 <phandb72@gmail.com>
2022-05-03 22:02:35 -0500

    Hashed Password Using bcrypt

--- auth.py
+++ auth.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from passlib.context import CryptContext
 from pydantic import BaseModel
 from typing import Optional
+
+from sqlalchemy.orm import Session
+
 import models
+from database import engine, SessionLocal
 
 
 class CreateUser(BaseModel):
@@ -12,11 +17,27 @@ class CreateUser(BaseModel):
     password: str
 
 
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
 
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+def get_password_hashed(password):
+    return bcrypt_context.hash(password)
+
+
 @app.post("/create/user")
-async def create_new_user(create_user: CreateUser):
+async def create_new_user(create_user: CreateUser, db: Session = Depends(get_db)):
     create_user_model = models.Users()
 
     create_user_model.email = create_user.email
@@ -24,7 +45,11 @@ async def create_new_user(create_user: CreateUser):
     create_user_model.first_name = create_user.first_name
     create_user_model.last_name = create_user.last_name
 
-    create_user_model.hashed_password = create_user.password
+    create_user_model.hashed_password = get_password_hashed(create_user.password)
     create_user_model.is_active = True
 
-    return create_user_model
+    db.add(create_user_model)
+    db.commit()
+
+    # return create_user_model
+
